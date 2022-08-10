e2e0f831077e4a92d273cce09fce4cc27afb4a46
Ashdown <rory.devane@gmail.com>
2022-08-10 11:10:38 +0100

    Render list of artists

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import useTracks from "./useTracks";
+import { Artist} from "./useTracks";
 
 const Page = () => {
 
   const tracks = useTracks()
 
   console.log('tracks', tracks)
+
+  const artists:Artist[] = []
+
+  tracks.forEach( track => {
+    if(!artists.find( artist => artist.id === track.artist.id)) {
+      artists.push(track.artist)
+    }
+  })
+
+  console.log('artists', artists)
 
   return (<div>
     <h1 data-testid="page-heading">Spotify App</h1>
@@ -19,6 +30,14 @@ const Page = () => {
     }
     </ul>
     <h2>Authors</h2>
+    <form>
+      { artists.map( artist =>
+      <label key={artist.id}>
+        <input type="radio" name="authors" value={artist.id}/>
+        {artist.name}
+      </label>
+      )}
+    </form>
   </div>)
 }
 
--- src/components/useTracks.ts
+++ src/components/useTracks.ts
@@ -1,9 +1,15 @@
 import useSpotifyTracks from "../services/useSpotifyTracks";
 
+export type Artist = {
+  id: string;
+  name: string;
+}
+
 type Track = {
   id: string;
   name: string;
   image: string;
+  artist: Artist;
 }
 
 type Tracks = Track[]
@@ -18,6 +24,10 @@ const useTracks = ():Tracks => {
     id: item.track.id,
     name: item.track.name,
     image: item.track.album.images.find( (image:any) => image.height === 64).url,
+    artist: {
+      id: item.track.artists[0].id,
+      name: item.track.artists[0].name
+    }
   }))
 
 }
