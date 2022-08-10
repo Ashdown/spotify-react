5ee05723e46bb9113924eafc5a795c26aa106598
Ashdown <rory.devane@gmail.com>
2022-08-10 11:53:33 +0100

    Filter tracks from the url query param

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import useTracks from "./useTracks";
 import Artists from "./Artists";
+import {useSearchParams} from "react-router-dom";
 
 const Page = () => {
 
+  const [searchParams] = useSearchParams();
   const tracks = useTracks()
+  const artist = searchParams.get('artist')
+  const filteredTracks = artist ? tracks.filter( track => track.artist.id === searchParams.get('artist')) : tracks
 
   return (<div>
     <h1 data-testid="page-heading">Spotify App</h1>
     <h2>Tracks</h2>
     <ul>
     {
-      tracks.map( track => <li key={track.id}>
+      filteredTracks.map( track => <li key={track.id}>
         {track.name}
         <img height="64" width="64" src={track.image}/>
       </li>)
